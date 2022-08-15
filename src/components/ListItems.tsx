86186450826568b0df1012c45c273345a1f98ff6
Fikri Bill Gufran <billgufran@gmail.com>
2022-08-15 14:08:48 +0800

    replace list item icon

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import LayersIcon from "@material-ui/icons/Layers";
+import WorkRoundedIcon from '@material-ui/icons/WorkRounded';
 import { Link as RouterLink } from "react-router-dom";
 
 export const mainListItems = (
   <div>
     <ListItem button component={RouterLink} to="/shift">
       <ListItemIcon>
-        <LayersIcon />
+        <WorkRoundedIcon />
       </ListItemIcon>
       <ListItemText primary="Shift" />
     </ListItem>
